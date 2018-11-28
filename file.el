895346fe08aa45161d755cacb78209cf9ccdce26
adnrbp <adnrbp@users.noreply.github.com>
2018-11-27 23:20:57 -0500

    simple Arithmetic and message

---
+++ file.el
@@ -0,0 +1,14 @@
+
+;C-x C-e ;eval-last-sexp
+;M-x ielm ;interactive elisp
+;M-x eval-region ; C-spc
+;C-h f ;M-x describe-function
+  
+;emacs --script file.el
+
+;simple arithmetic
+(+ 3 4)
+;print
+(message "appear")
+
+
